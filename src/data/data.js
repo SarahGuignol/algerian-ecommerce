588770b0ad2ab1c091928424a3f79ab28147af8d
SarahGuignol <sarah.guignol@univ-alger.dz>
2025-05-08 03:18:29 +0100

    Added product images to public folder and updated references

--- src/data/data.js
+++ src/data/data.js
@@ -1,47 +1,281 @@
 export const products = [
     {
       id: 1,
-      name: "Traditional Algerian Kaftan",
-      image: "https://example.com/kaftan.jpg",
-      price: 12000,
+      name: "Traditional Algerian white Caftan",
+      image: "/images/caftan1.jpg",
+      price: 45000,
       views: 0,
       sold: 0,
       reviews: []
     },
     {
-      id: 2,
-      name: "Algerian Berber Dress",
-      image: "https://example.com/berber-dress.jpg",
-      price: 8500,
-      views: 0,
-      sold: 0,
-      reviews: []
+        id: 2,
+        name: "Traditional Algerian black Caftan",
+        image: "/images/caftan2.jpg",
+        price: 30000,
+        views: 0,
+        sold: 0,
+        reviews: []
     },
     {
-      id: 3,
-      name: "Algiers T-Shirt",
-      image: "https://example.com/algiers-tshirt.jpg",
-      price: 2500,
-      views: 0,
-      sold: 0,
-      reviews: []
+        id: 3,
+        name: "Traditional Algerian green Caftan",
+        image: "/images/caftan3.jpg",
+        price: 60000,
+        views: 0,
+        sold: 0,
+        reviews: []
     },
     {
       id: 4,
-      name: "Sahara Scarf",
-      image: "https://example.com/sahara-scarf.jpg",
-      price: 3500,
+      name: "Kabyle Dress",
+      image: "/images/jebaqbayel1.jpg",
+      price: 25000,
       views: 0,
       sold: 0,
       reviews: []
     },
     {
-      id: 5,
-      name: "Kabyle Jewelry Set",
-      image: "https://example.com/kabyle-jewelry.jpg",
-      price: 6500,
+        id: 5,
+        name: "Kabyle Dress for bride",
+        image: "/images/jebaqbayel2.jpg",
+        price: 25000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+      id: 6,
+      name: "Algiers green karakou jacket",
+      image: "/images/karakoujaket1.jpg",
+      price: 9000,
       views: 0,
       sold: 0,
       reviews: []
+    },
+    {
+        id: 7,
+        name: "Algiers blue karakou jacket",
+        image: "/images/karakoujaket2.jpg",
+        price: 16000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 8,
+        name: "Chaoui Dress for bride",
+        image: "/images/chaoui1.jpg",
+        price: 48000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 9,
+        name: "Chaoui Dress",
+        image: "/images/chaoui2.jpg",
+        price: 29000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 10,
+        name: "Nayli Dress",
+        image: "/images/jebanayli1.jpg",
+        price: 19000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 11,
+        name: "Nayli Dress for bride",
+        image: "/images/jebanayli2.jpg",
+        price: 30000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 12,
+        name: "Algerian blue Karakou",
+        image: "/images/karakou1.jpg",
+        price: 49000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 13,
+        name: "Algerian black Karakou for bride",
+        image: "/images/karakou2.jpg",
+        price: 80000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 14,
+        name: "Algerian black Karakou",
+        image: "/images/karakou3.jpg",
+        price: 70000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 15,
+        name: "Algerian jabador for woman",
+        image: "/images/jabador.jpg",
+        price: 4500,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 16,
+        name: "Algerian Barnous for man",
+        image: "/images/barnousM.jpg",
+        price: 7000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 17,
+        name: "Algerian Barnous for woman",
+        image: "/images/barnousW.jpg",
+        price: 9000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 18,
+        name: "Algerian Hyek mterraz",
+        image: "/images/hayek1.jpg",
+        price: 5000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 19,
+        name: "Algerian Hyek",
+        image: "/images/hayek2.jpg",
+        price: 3000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+      id: 20,
+      name: "Algrian Scarf for wedding",
+      image: "/images/scarf1.jpg",
+      price: 3200,
+      views: 0,
+      sold: 0,
+      reviews: []
+    },
+    {
+        id: 21,
+        name: "Algrian Handmade Scarf for bride",
+        image: "/images/scarf2.jpg",
+        price: 12000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+      id: 22,
+      name: "Kabyle Jewelry",
+      image: "/images/jewelry3.jpg",
+      price: 23000,
+      views: 0,
+      sold: 0,
+      reviews: []
+    },
+    {
+        id: 23,
+        name: "Kabyle Morjan Jewelry",
+        image: "/images/jewelry4.jpg",
+        price: 32000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 24,
+        name: "Handmade Jewelry",
+        image: "/images/jewelry1.jpg",
+        price: 30000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 25,
+        name: "Handmade Nayli Jewelry",
+        image: "/images/jewelry2.jpg",
+        price: 30000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 26,
+        name: "Algiers Jewelry for married woman",
+        image: "/images/khitrrouh1.jpg",
+        price: 13000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 27,
+        name: "Algiers Jewelry for single woman",
+        image: "/images/khitrrouh2.jpg",
+        price: 9000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 28,
+        name: "Bag for karakou",
+        image: "/images/bag1.jpg",
+        price: 5000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 29,
+        name: "Handmade Bag",
+        image: "/images/bag2.jpg",
+        price: 6000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 30,
+        name: "Traditional shoes for man",
+        image: "/images/shoesM.jpg",
+        price: 4000,
+        views: 0,
+        sold: 0,
+        reviews: []
+    },
+    {
+        id: 31,
+        name: "Traditional shoes for woman",
+        image: "/images/shoesW.jpg",
+        price: 9800,
+        views: 0,
+        sold: 0,
+        reviews: []
     }
   ];
